40efceaa0498c6a362057e1d76c1ccfb0abb6a10
Andrew Wei <andrewscwei@gmail.com>
2021-07-31 11:58:44 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,22 +10,19 @@ import Darwin.C
 #endif
 
 enum Environment: String {
-  static let `default`: Environment = .local
-
   case local
   case development
   case production
 
   static func get() -> Environment {
-    if let envPointer = getenv("CI"), String(cString: envPointer) == "true" {
+    if let envPointer = getenv("SWIFT_ENV"), let environment = Environment(rawValue: String(cString: envPointer)) {
+      return environment
+    }
+    else if let envPointer = getenv("CI"), String(cString: envPointer) == "true" {
       return .production
     }
-    else if let envPointer = getenv("SWIFT_ENV") {
-      let env = String(cString: envPointer)
-      return Environment(rawValue: env) ?? .default
-    }
     else {
-      return .default
+      return .local
     }
   }
 }
