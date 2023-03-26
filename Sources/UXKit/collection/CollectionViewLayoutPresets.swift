6f14ab8a242f440a11211fb4af63c70d879bdac4
Andrew Wei <andrew@sybl.io>
2023-03-26 12:37:01 -0700

    Refactor

--- Sources/UXKit/collection/CollectionViewLayoutPresets.swift
+++ Sources/UXKit/collection/CollectionViewLayoutPresets.swift
@@ -5,7 +5,10 @@ import UIKit
 public struct CollectionViewLayoutPresets {
 
   @available(iOS 14.5, *)
-  public static func listLayout(separatorColor: UIColor? = nil, separatorInsets: NSDirectionalEdgeInsets? = nil) -> UICollectionViewCompositionalLayout {
+  public static func listLayout(
+    separatorColor: UIColor? = nil,
+    separatorInsets: NSDirectionalEdgeInsets? = nil
+  ) -> UICollectionViewCompositionalLayout {
     var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
     configuration.backgroundColor = .clear
 
@@ -20,19 +23,27 @@ public struct CollectionViewLayoutPresets {
     return UICollectionViewCompositionalLayout.list(using: configuration)
   }
 
-  public static func horizontalLayout(sectionContentInsets: NSDirectionalEdgeInsets = .zero, itemContentInsets: NSDirectionalEdgeInsets = .zero) -> UICollectionViewCompositionalLayout {
+  public static func linearLayout(
+    direction: UICollectionView.ScrollDirection = .horizontal,
+    itemSize: NSCollectionLayoutSize = .init(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)),
+    itemContentInsets: NSDirectionalEdgeInsets = .zero,
+    groupSize: NSCollectionLayoutSize = .init(widthDimension: .fractionalHeight(1.0), heightDimension: .fractionalHeight(1.0)),
+    groupContentInsets: NSDirectionalEdgeInsets = .zero,
+    sectionContentInsets: NSDirectionalEdgeInsets = .zero,
+    interGroupSpacing: CGFloat = 0.0
+  ) -> UICollectionViewCompositionalLayout {
     let config = UICollectionViewCompositionalLayoutConfiguration()
-    config.scrollDirection = .horizontal
+    config.scrollDirection = direction
 
-    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
     let item = NSCollectionLayoutItem(layoutSize: itemSize)
     item.contentInsets = itemContentInsets
 
-    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalHeight(1.0), heightDimension: .fractionalHeight(1.0))
-    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+    let group = direction == .horizontal ? NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item]) : NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+    group.contentInsets = groupContentInsets
 
     let section = NSCollectionLayoutSection(group: group)
     section.contentInsets = sectionContentInsets
+    section.interGroupSpacing = interGroupSpacing
 
     let layout = UICollectionViewCompositionalLayout(section: section, configuration: config)
 
