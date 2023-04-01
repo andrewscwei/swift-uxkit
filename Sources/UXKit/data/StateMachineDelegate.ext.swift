69e67710a560bf2a26a505639d05cbb95d8aa30e
Andrew Wei <andrew@sybl.io>
2023-04-01 10:13:12 -0700

    Update docs

--- Sources/UXKit/data/StateMachineDelegate.ext.swift
+++ Sources/UXKit/data/StateMachineDelegate.ext.swift
@@ -4,8 +4,7 @@ import BaseKit
 import UIKit
 
 extension StateMachineDelegate where Self: UIViewController {
-
-  public func observe<T: Equatable>(_ liveData: LiveData<T>, invalidate keyPath: AnyKeyPath) {
+  public func observe<T>(_ liveData: LiveData<T>, invalidate keyPath: AnyKeyPath) {
     liveData.observe(for: self) { _ in
       DispatchQueue.main.async {
         self.stateMachine.invalidate(keyPath)
@@ -13,7 +12,7 @@ extension StateMachineDelegate where Self: UIViewController {
     }
   }
 
-  public func observe<T: Equatable>(_ liveData: LiveData<T>, invalidate stateType: StateType) {
+  public func observe<T>(_ liveData: LiveData<T>, invalidate stateType: StateType) {
     liveData.observe(for: self) { _ in
       DispatchQueue.main.async {
         self.stateMachine.invalidate(stateType)
@@ -21,7 +20,7 @@ extension StateMachineDelegate where Self: UIViewController {
     }
   }
 
-  public func unobserve<T: Equatable>(_ liveData: LiveData<T>) {
+  public func unobserve<T>(_ liveData: LiveData<T>) {
     liveData.unobserve(for: self)
   }
 }
